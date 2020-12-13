56f2d4e2f50720e7c32b397566b2eec71be47ef4
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-12-13 11:53:41 -0600

    finished commenting server side and all client side minus the admin page

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -5,60 +5,62 @@ const pool = require('../modules/pool');
 
 // GET ROUTE
 router.get('/', (req, res) => {
+    // pulls all info from the database and orders it by date
     let sqlText = `SELECT * FROM "feedback" ORDER BY "date" DESC;`;
     pool.query(sqlText)
     .then((result) => {
-        res.send(result.rows);
-    })
-    .catch((error) => {
+        res.send(result.rows); // all the data stored in the database is sent to client-side
+    }).catch((error) => {
         console.log('Bad new bears getting the data...', error);
         res.sendStatus(500);
     });
-});
+}); // end GET ROUTE
   
 // POST ROUTE
 router.post('/',  (req, res) => {
-    let feedback = req.body;
+    let feedback = req.body; // this is all of the feedback information 
     console.log(`Adding feedback`, feedback);
     
+    // all of the feedback information is sent to the database
     let sqlText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
                        VALUES ($1, $2, $3, $4);`;
     pool.query(sqlText, [feedback.feelings, feedback.understanding, feedback.support, feedback.comments])
         .then((result) => {
-            res.sendStatus(201);
-        })
-        .catch((error) => {
+            res.sendStatus(201); // returns an OK, it was added
+        }).catch((error) => {
             console.log('Bad new bears getting the data...', error);
             res.sendStatus(500);
         });
-}); 
+}); // end POST ROUTE
 
-// PUT Route
+// PUT ROUTE
 router.put('/:id', (req, res) => {
-    let id = req.params.id; 
-    const sqlText = `UPDATE "feedback" SET "flagged" = true WHERE id = $1;`;
-    
+    let id = req.params.id; // identifys which item to change by id
+
+    // updates whether an item is flagged or not in the database from default - false to true
+    let sqlText = `UPDATE "feedback" SET "flagged" = true WHERE id = $1;`;
     pool.query(sqlText, [id])
         .then((result) => {
-            res.sendStatus(200); // sends an OK - this was updated in DB to client-side
+            res.sendStatus(200); // sends an OK - this was updated in database to client-side
         }).catch((error) => {
-            console.log('Error when adding likes...', error)
-            res.sendStatus(500); // Good server always responds
-        }) 
-}); // END PUT Route
-
-// DELETE Route
-router.delete('/:id', (req, res) => {
-    let id = req.params.id; 
-    const sqlText = `DELETE FROM "feedback" WHERE id=$1;`
-    pool.query(sqlText, [id]) 
-        .then((result) => { 
-            res.sendStatus(200); 
-        })
-        .catch((error) => {
             console.log('Bad new bears getting the data...', error);
             res.sendStatus(500);
         });
-}) // DELETE Route
+}); // END PUT ROUTE
+
+// DELETE ROUTE
+router.delete('/:id', (req, res) => {
+    let id = req.params.id; // identifys which item to change by id
+
+    // deletes an item from the database
+    let sqlText = `DELETE FROM "feedback" WHERE id=$1;`
+    pool.query(sqlText, [id]) 
+        .then((result) => { 
+            res.sendStatus(200); // sends an OK - this was updated in database to client-side
+        }).catch((error) => {
+            console.log('Bad new bears getting the data...', error);
+            res.sendStatus(500);
+        });
+}) // end DELETE ROUTE
 
 module.exports = router;
--- src/components/Submitted/Submitted.js
+++ src/components/Submitted/Submitted.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'; 
 import './Submitted.css';
 
+// import material UI
 import { StylesProvider } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
@@ -11,26 +12,33 @@ import CardContent from '@material-ui/core/CardContent';
 class SubmitSuccess extends Component {
 
   goToBeginning = () => {
+    // routes the user to the next component
     this.props.history.push("/");
-  }
+  } // end goToBeginning
 
   render() {
     return (
       <div className="center">
         <StylesProvider injectFirst>
-        <Card>
-          <CardContent className="cardContent">
-            <h2>Thank you for your Feedback!</h2>
-          </CardContent>
-          <CardActions className="button">
-          <Button variant="outlined" color="primary" className="button"
-              onClick={this.goToBeginning}>Retake the Feedback Survey!</Button>
-          </CardActions>
-        </Card>
+          <Card>
+            <CardContent className="cardContent">
+              <h2>Thank you for your Feedback!</h2>
+            </CardContent>
+            <CardActions className="button">
+              <Button 
+                  variant="outlined" 
+                  color="primary" 
+                  className="button"
+                  onClick={this.goToBeginning}>
+                    Retake the Feedback Survey!
+              </Button>
+            </CardActions>
+          </Card>
         </StylesProvider>
       </div>
     );
   }
 }
 
+// withRouter sends user to next component
 export default withRouter(SubmitSuccess);
